10fe09c641abdbd7e19e45770d79c4f7b2159dec
Lubos Krnac <lubos.krnac@gmail.com>
2020-05-29 11:11:32 +0200

    Submit coverage report to CodeCov.io
    VTMS-2541

--- jest.config.js
+++ jest.config.js
@@ -10,13 +10,5 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|\\.(test|spec))\\.jsx?$',
     moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
-    coverageThreshold: {
-        global: {
-            branches: 30,
-            functions: 90,
-            lines: 90,
-            statements: 90,
-        },
-    },
     setupFiles: ['./setupTests'],
 };
